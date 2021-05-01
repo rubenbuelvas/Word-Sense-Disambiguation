65267fe660aa1694af78c76eae6091882efcf6d0
rubenbuelvas <rcbuelvasv@gmail.com>
2021-05-01 17:29:57 -0400

    generate dataset csv

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import numpy as np
+import csv
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import CategoricalNB
+
 # Feature extraction
 
-CATEGORIES_STOPWORDS = ['./.', ',/,', '[', ']', '\n']
+CATEGORIES_STOPWORDS = ['./.', ',/,', '[', ']', '\n', "'", "''", '``']
 
 
-def categories_extraction(n_words=2):
+def gc_extraction(n_words=2):
+    data = []
     with open('interest.acl94.txt') as file:
         lines = file.readlines()
     separator = lines[1]
@@ -15,31 +21,52 @@ def categories_extraction(n_words=2):
         for _ in range(n_words):
             nulls.append('/NULL')
         line = nulls + line + nulls
+        target_word_found = False
         for i in range(len(line)):
             if line[i].find('interest_') == 0:
                 category = line[i][9:10]
-                line = line[i-n_words:i+n_words+1]
+                line = line[i - n_words:i + n_words + 1]
+                target_word_found = True
                 break
-        line.pop(n_words)
-        for i in range(len(line)):
-            line[i] = line[i].split('/')[1]
-        line.append(category)
-        print(line)
-        break
+        if target_word_found:
+            line.pop(n_words)
+            for i in range(len(line)):
+                try:
+                    line[i] = line[i].split('/')[1]
+                except:
+                    line[i] = 'NULL'
+            line.insert(0, category)
+            data.append(line)
+    with open('gc_dataset.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
 
-# Naive Bayes Training
+ENGLISH_STOPWORDS = []
 
-# Naive Bayes Test
 
-# Decision Tree Training
+def bow_extraction():
+    pass
 
-# Decision Tree Test
 
-# MultiLayer Perceptron Training
+def load_dataset(filename, test_size=0.2):
+    with open(filename) as file:
+        file.readline()  # Ignore first line
+        data = np.loadtxt(file)
+    X = data[:, 1:]
+    y = data[:, 0]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=0)
+    return X_train, X_test, y_train, y_test
 
-# MultiLayer Perceptron Test
+# Naive Bayes
+
+def train_naive_bayes(dataset):
+    pass
+
+# Decision Tree
+
+# MultiLayer Perceptron
 
 
 if __name__ == '__main__':
-    categories_extraction()
+    gc_extraction()
